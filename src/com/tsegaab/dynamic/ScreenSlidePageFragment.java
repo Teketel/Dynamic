f064ea18fb3497400969b238d7cd37f660dbc654
teketel <for.teketel@gmail.com>
2014-01-29 12:25:41 +0300

    update

---
+++ src/com/tsegaab/dynamic/ScreenSlidePageFragment.java
@@ -0,0 +1,91 @@
+package com.tsegaab.dynamic;
+
+
+import java.io.File;
+import java.util.ArrayList;
+
+import com.tsegaab.dynamic.objects.Article;
+
+import android.os.Bundle;
+import android.annotation.SuppressLint;
+import android.app.Fragment;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.widget.ImageView;
+import android.widget.TextView;
+ 
+
+@SuppressLint("ValidFragment")
+public class ScreenSlidePageFragment extends Fragment {
+
+	
+	public static final String ARTICLE_ID = "ID";
+	private int article_index;
+	private Article article;
+	
+	public ScreenSlidePageFragment() {
+		
+	}
+	public ScreenSlidePageFragment(Article article) {
+		this.article = article;
+    }
+	
+	@Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        article_index = getArguments().getInt(ARTICLE_ID);
+    }
+	
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		ViewGroup articleView = (ViewGroup) inflater.inflate(
+				R.layout.single_article, container, false);
+		TextView article_title = ((TextView) articleView
+				.findViewById(R.id.sigle_article_title));
+//		Typeface article_title_font = Typeface.createFromAsset(getAssets(),
+//				"fonts/NYALA.TTF");
+//		article_title.setTypeface(article_title_font);
+		article_title.setText(this.article.getTitle());
+
+		ImageView article_image = (ImageView) articleView
+				.findViewById(R.id.single_article_image);
+		if (this.article.getImage_local_path() != null) {
+			File imgFile = new  File(this.article.getImage_local_path());
+			if(imgFile.exists()){
+			    Bitmap bmp = BitmapFactory.decodeFile(imgFile.getAbsolutePath());
+			    article_image.setImageBitmap(bmp);
+			}
+			
+		}
+		
+		
+		WebView article_full_content = ((WebView) articleView
+				.findViewById(R.id.article_full_content));
+		WebSettings settings = article_full_content.getSettings();
+		settings.setDefaultTextEncodingName("utf-8");
+		article_full_content.loadData(this.article.getStyled_FullContent(), "text/html; charset=utf-8",
+				"UTF-8");
+
+		return articleView;
+	}
+	
+	public static ScreenSlidePageFragment create(ArrayList<Article> articles, int pageNumber) {
+        ScreenSlidePageFragment fragment = new ScreenSlidePageFragment(articles.get(articles.size() - (pageNumber + 1)));
+        Bundle args = new Bundle();
+        args.putInt(ARTICLE_ID, pageNumber);
+        fragment.setArguments(args);
+        Log.d(Consts.XML_NAME_SPACE, "Position " + pageNumber);
+        return  fragment;
+    }
+	
+	public int getPageNumber() {
+        return article_index;
+    }
+}
---
+++ src/com/tsegaab/dynamic/SingleArticleActivity.java
@@ -0,0 +1,75 @@
+package com.tsegaab.dynamic;
+
+import java.util.ArrayList;
+
+import com.tsegaab.dynamic.objects.Article;
+
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v13.app.FragmentStatePagerAdapter;
+
+
+public class SingleArticleActivity extends FragmentActivity {
+
+	private static int NUM_PAGES;
+	private ViewPager mPager;
+	private PagerAdapter mPagerAdapter;
+	private ArrayList<Article> articles_to_display;
+	private int article_index;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_screen_slide);
+
+		Intent i = getIntent();
+		article_index = i.getIntExtra("article_index", 0);
+
+		articles_to_display = Consts.current_articles;
+		NUM_PAGES = articles_to_display.size();
+
+		// Instantiate a ViewPager and a PagerAdapter.
+		mPager = (ViewPager) findViewById(R.id.pager);
+		mPagerAdapter = new ScreenSlidePagerAdapter(getFragmentManager());
+		mPager.setAdapter(mPagerAdapter);
+		mPager.setCurrentItem(NUM_PAGES - (article_index + 1));
+		mPager.setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener() {
+			@Override
+			public void onPageSelected(int position) {
+				// When changing pages, reset the action bar actions since they
+				// are dependent
+				// on which page is currently active. An alternative approach is
+				// to have each
+				// fragment expose actions itself (rather than the activity
+				// exposing actions),
+				// but for simplicity, the activity provides the actions in this
+				// sample.
+				position = article_index;
+				invalidateOptionsMenu();
+			}
+		});
+	}
+
+	private class ScreenSlidePagerAdapter extends FragmentStatePagerAdapter {
+		public ScreenSlidePagerAdapter(FragmentManager fm) {
+			super(fm);
+		}
+
+		@Override
+		public Fragment getItem(int position) {
+			return ScreenSlidePageFragment
+					.create(articles_to_display, position);
+			
+		}
+
+		@Override
+		public int getCount() {
+			return NUM_PAGES;
+		}
+	}
+}
